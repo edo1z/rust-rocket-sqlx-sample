84ccb41e5cfad58f04ce5ae3ea64b4ba044ab989
net3i <web3ten0@gmail.com>
2023-09-19 11:27:15 +0900

    usecaseをtrait化

--- src/main.rs
+++ src/main.rs
@@ -11,20 +11,23 @@ use config::Config;
 use db::Db;
 use dotenv::dotenv;
 use rocket::fairing::AdHoc;
+use rocket::State;
 use rocket_db_pools::{Connection, Database};
 
 #[get("/")]
-async fn index(db_con: Connection<Db>) -> Result<String, String> {
+async fn index(db_con: Connection<Db>, app: &State<AppState>) -> Result<String, String> {
     let mut pool = db_con.into_inner();
-    usecase::get_all_products_and_users(&mut pool)
+    app.usecase
+        .get_all_products_and_users(&mut pool)
         .await
         .map_err(|_| "error".to_string())
 }
 
 #[post("/new")]
-async fn add(db_con: Connection<Db>) -> Result<String, String> {
+async fn add(db_con: Connection<Db>, app: &State<AppState>) -> Result<String, String> {
     let mut pool = db_con.into_inner();
-    usecase::create_product_and_user(&mut pool)
+    app.usecase
+        .create_product_and_user(&mut pool)
         .await
         .map_err(|_| "error".to_string())
 }
@@ -34,11 +37,21 @@ async fn hoge() -> &'static str {
     "hoge"
 }
 
+pub struct AppState {
+    pub usecase: Box<dyn usecase::UseCase + Send + Sync>,
+}
+
 #[launch]
 async fn rocket() -> _ {
     dotenv().ok();
 
+    let usecase_impl = Box::new(usecase::UseCaseImpl::new());
+    let app_state = AppState {
+        usecase: usecase_impl,
+    };
+
     rocket::build()
+        .manage(app_state)
         .attach(Db::init())
         .attach(AdHoc::config::<Config>())
         .mount("/", routes![index, add, hoge])
--- src/usecase.rs
+++ src/usecase.rs
@@ -1,27 +1,50 @@
+use crate::repo;
 use sqlx::pool::PoolConnection;
 use sqlx::Acquire;
 use sqlx::Postgres;
 
-use crate::repo;
-
-pub async fn get_all_products_and_users(
-    pool: &mut PoolConnection<Postgres>,
-) -> Result<String, sqlx::Error> {
-    let products = repo::find_all_products(&mut *pool).await?;
-    let users = repo::find_all_users(&mut *pool).await?;
-    Ok(format!("products: {:?} usres: {:?}", products, users))
+#[async_trait]
+pub trait UseCase {
+    async fn get_all_products_and_users(
+        &self,
+        pool: &mut PoolConnection<Postgres>,
+    ) -> Result<String, sqlx::Error>;
+    async fn create_product_and_user(
+        &self,
+        pool: &mut PoolConnection<Postgres>,
+    ) -> Result<String, sqlx::Error>;
 }
 
-pub async fn create_product_and_user(
-    pool: &mut PoolConnection<Postgres>,
-) -> Result<String, sqlx::Error> {
-    let mut txn = pool.begin().await?;
+pub struct UseCaseImpl;
+impl UseCaseImpl {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
 
-    let user = repo::create_user(&mut txn).await?;
-    let product = repo::create(&mut txn).await?;
+#[async_trait]
+impl UseCase for UseCaseImpl {
+    async fn get_all_products_and_users(
+        &self,
+        pool: &mut PoolConnection<Postgres>,
+    ) -> Result<String, sqlx::Error> {
+        let products = repo::find_all_products(&mut *pool).await?;
+        let users = repo::find_all_users(&mut *pool).await?;
+        Ok(format!("products: {:?} usres: {:?}", products, users))
+    }
 
-    // txn.rollback().await.unwrap();
-    txn.commit().await.unwrap();
+    async fn create_product_and_user(
+        &self,
+        pool: &mut PoolConnection<Postgres>,
+    ) -> Result<String, sqlx::Error> {
+        let mut txn = pool.begin().await?;
 
-    Ok(format!("product:{:?}, user:{:?}", product, user))
+        let user = repo::create_user(&mut txn).await?;
+        let product = repo::create(&mut txn).await?;
+
+        // txn.rollback().await.unwrap();
+        txn.commit().await.unwrap();
+
+        Ok(format!("product:{:?}, user:{:?}", product, user))
+    }
 }
